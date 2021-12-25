72d0c5710b65fe274b942158b93bed3d89cdc4f4
quoc thai <hoquocthai204@gmail.com>
2021-12-25 23:02:48 +0700

    first commit

---
+++ src/components/home/filter/filter.tsx
@@ -0,0 +1,7 @@
+import './filter.scss'
+
+function Filter(){
+    return <></>
+}
+
+export {Filter}
--- src/components/home/sidebar.tsx
+++ src/components/home/sidebar.tsx
@@ -1,55 +1,53 @@
 
 import pointer from '../imgs/pointer.png'
 import Logo from '../logo/logo'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useState, useRef, useEffect } from 'react'
 
-function Sidebar(){
-    const activeref = useRef<HTMLDivElement|null>(null)
-    const [tag, setTag] = useState<HTMLElement|null>(null)
+function Sidebar() {
+    const [active, setActive] = useState('home')
+
     useEffect(()=>{
-        tag?.parentElement?.parentElement?.classList.add('active')
-        return ()=>{
-            tag?.parentElement?.parentElement?.classList.remove('active')
+        let element = document.querySelector(`.${active}`) as HTMLElement
+        element.classList.add('active')
+
+        return()=>{
+            element.classList.remove('active')
         }
-    },[tag])
-
-    function handleClick(e:any){
-        setTag(e.target)
-    }
-
-    return(
+    },[active])
+    
+    return (
         <div className="sidebar">
-            <Logo/>
+            <Logo />
 
-            <div className='home icon' ref={activeref} onClick={(handleClick)}>
+            <div className='home icon active' onClick={()=>setActive('home')}>
                 <Link to="/"><i className="fas fa-home"></i></Link>
                 <img className='pointer' src={pointer} alt='' />
             </div>
 
-            <div className='import icon' onClick={handleClick}>
+            <div className='import icon' onClick={()=>setActive('import')}>
                 <Link to='/import'><i className="fas fa-file-import"></i></Link>
-                <img className='pointer' src={pointer} alt=''/>
+                <img className='pointer' src={pointer} alt='' />
             </div>
 
-            <div className='conversation icon' onClick={handleClick}>
+            <div className='conversation icon' onClick={()=>setActive('conversation')}>
                 <Link to='/conversation'><i className="fas fa-comments"></i></Link>
-                <img className='pointer' src={pointer} alt=''/>
+                <img className='pointer' src={pointer} alt='' />
             </div>
 
-            <div className='group icon' onClick={handleClick}>
+            <div className='group icon' onClick={()=>setActive('group')}>
                 <Link to='/group'><i className="far fa-folder-open"></i></Link>
-                <img className='pointer' src={pointer} alt=''/>
+                <img className='pointer' src={pointer} alt='' />
             </div>
 
-            <div className='trash icon' onClick={handleClick}>
+            <div className='trash icon' onClick={()=>setActive('trash')}>
                 <Link to='/trash'><i className="fas fa-trash-alt"></i></Link>
-                <img className='pointer' src={pointer} alt=''/>
+                <img className='pointer' src={pointer} alt='' />
             </div>
-            
-            <div className='gear icon' onClick={handleClick}>
+
+            <div className='gear icon' onClick={()=>setActive('gear')}>
                 <Link to='/gear'><i className="fas fa-cog"></i></Link>
-                <img className='pointer' src={pointer} alt=''/>
+                <img className='pointer' src={pointer} alt='' />
             </div>
         </div>
     )
